464fab6aecdb2a6af6c7958ea0509c98b8fc5cba
Rishit999 <rishitnayakar@gmail.com>
2025-10-31 08:46:25 +0530

    saving the file to git hub

---
+++ add.py
@@ -0,0 +1,8 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+sum_result = num1 + num2
+
+print("The sum of", num1, "and", num2, "is", sum_result)
